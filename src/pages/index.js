e3b17da17324bc47eecc4ce7f781e4cf4876644f
Kurtis <kcasperson7@gmail.com>
2023-05-22 09:09:43 -0700

    Update headline

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Software Developer Portfolio" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="Who am I !?" />
